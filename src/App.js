30b6ac51fe411f3b7d9b9860ffc68cb13cedfc9e
FourSqweez <four7518@gmail.com>
2022-08-19 23:54:19 +0700

    Update : get token from location.hash and put it to context with dispatch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Login from './components/Login'
+import { reducerCases } from './utils/Contants'
+import { useStateProvider } from './utils/StateProvider'
 
 export default function App() {
-	return <div>App</div>
+	const [{ token }, dispatch] = useStateProvider()
+	useEffect(() => {
+		const hash = window.location.hash
+		if (hash) {
+			const token = hash.substring(1).split('&')[0].split('=')[1]
+			// console.log(token)
+			dispatch({ action: reducerCases.SET_TOKEN, token })
+		}
+	}, [token, dispatch])
+	return (
+		<div>
+			<Login />
+		</div>
+	)
 }
