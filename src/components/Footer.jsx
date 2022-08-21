650e7b09c027baaa97c2d491ed949db814d34993
FourSqweez <four7518@gmail.com>
2022-08-21 15:21:22 +0700

    Add : Created new components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled'
+import React from 'react'
+
+export default function Footer() {
+	return <Container>Footer</Container>
+}
+
+const Container = styled.div`
+	background-color: #181818;
+	height: 100%;
+`
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+import styled from '@emotion/styled'
+import React from 'react'
+
+export default function Navbar() {
+	return <Container>Navbar</Container>
+}
+const Container = styled.div``
